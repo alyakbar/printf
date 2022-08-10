22873e3f7d1bb7bad7313be4b2601a704f639e86
Alyakbar Ahmed <74308591+alyakbar@users.noreply.github.com>
2022-08-10 07:57:54 -0700

    Update printf_char.c

--- printf_char.c
+++ printf_char.c
@@ -1,15 +1,38 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
+ * print_c - prints char
+ * @c: char to be printed
+ *
+ * Return: always 1
  */
-int printf_char(va_list val)
-{
-	char s;
 
-	s = va_arg(val, int);
-	_putchar(s);
+int print_c(va_list c)
+{
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
 	return (1);
+}
+
+/**
+ * print_s - prints a string
+ * @s: string to be printed
+ *
+ * Return: number of chars printed
+ */
+
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
