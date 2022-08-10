1f769fac9996db259b74d0c8c9f013beae080a5e
Alyakbar Ahmed <74308591+alyakbar@users.noreply.github.com>
2022-08-10 07:55:13 -0700

    Update printf_nos.c

--- printf_nos.c
+++ printf_nos.c
@@ -1,35 +1,122 @@
-#include "holberton.h"
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@alyakbar 
+Joseph-Gicuguma
+/
+printf
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+printf/print_nos.c
+@InnocentWahome
+InnocentWahome add prototype
+Latest commit 20997a7 4 days ago
+ History
+ 1 contributor
+79 lines (75 sloc)  1.08 KB
+
+#include "main.h"
 
 /**
- * printf_oct - prints an octal number.
- * @val: arguments.
- * Return: counter.
+ * print_i - prints an integer
+ * @i: integer to printed
+ *
+ * Return: number of chars and digits printed
  */
-int printf_oct(va_list val)
+int print_i(va_list i)
 {
-	int i;
-	int *array;
-	int counter = 0;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int temp = num;
+	int a[10];
+	int j, m, n, sum, count;
 
-	while (num / 8 != 0)
+	n = va_arg(i, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
 	{
-		num /= 8;
-		counter++;
+		m /= 10;
+		a[j] = (n / m) % 10;
 	}
-	counter++;
-	array = malloc(counter * sizeof(int));
-
-	for (i = 0; i < counter; i++)
+	if (n < 0)
 	{
-		array[i] = temp % 8;
-		temp /= 8;
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
 	}
-	for (i = counter - 1; i >= 0; i--)
+	for (j = 0, sum = 0; j < 10; j++)
 	{
-		_putchar(array[i] + '0');
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
 	}
-	free(array);
-	return (counter);
+	return (count);
 }
+
+/**
+ * print_d - prints a decimal
+ * @d: decimal to be printed
+ *
+ * Return: number of chars and digits printed
+ */
+int print_d(va_list d)
+{
+	int a[10];
+	int j, m, n, sum, count;
+
+	n = va_arg(d, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+You have no unread notifications
